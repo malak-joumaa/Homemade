beb16c017e2b4d7532335e094dfebabfeffafd2e
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-05 13:24:36 +0300

    Node.js: Adding express, cors, and the port to listen to

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,13 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
 
 const DB_CONNECT = process.env.DB_CONNECT || "";
 mongoose.connect(DB_CONNECT, () => console.log("connected to db"));
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, () => console.log("Server running"));
