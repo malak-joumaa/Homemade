c0dfd78f018b8c905fafdb2dc59ea503f6538257
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-06 16:58:14 +0300

    Backend: Adding userRouter

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 require("dotenv").config();
-const mongoose = require("mongoose");
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+const userRouter = require("./src/user");
 
+// mongodDB online -> use Atlas
 const DB_CONNECT = process.env.DB_CONNECT || "";
-mongoose.connect(DB_CONNECT, () => console.log("connected to db"));
-
+mongoose.connect(DB_CONNECT, (err) => {
+  if (err) console.log(err);
+  else console.log("Connected to db");
+});
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use("/api", userRouter);
 app.listen(5000, () => console.log("Server running"));
