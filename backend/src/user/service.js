50fb885adb6f439b78bbb72a1cbf2f627a25f05a
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-06 16:39:41 +0300

    Backend: Adding user_type to addUser function

---
+++ backend/src/user/service.js
@@ -0,0 +1,60 @@
+const User = require("../../model/Users");
+const Cook = require("../../model/Cooks");
+const Customer = require("../../model/Customers");
+
+//Add user function
+async function addUser(body, hashPassword) {
+  const {
+    first_name,
+    last_name,
+    email,
+    phone_number,
+    profile_photo,
+    location,
+    user_type,
+  } = body;
+
+  const user = new User({
+    first_name,
+    last_name,
+    email,
+    password: hashPassword,
+    phone_number,
+    profile_photo,
+    location,
+    user_type,
+  });
+
+  return await user.save();
+}
+
+//Add cook function
+async function addCook(body) {
+  const { rate, opening_hours, user } = body;
+
+  const cook = new Cook({
+    rate,
+    opening_hours,
+    user,
+  });
+
+  return await cook.save();
+}
+
+//Add customer function
+async function addCustomer(body) {
+  const { user, categories } = body;
+
+  const customer = new Customer({
+    user,
+    categories,
+  });
+
+  return await customer.save();
+}
+
+module.exports = {
+  addUser,
+  addCook,
+  addCustomer,
+};
