0ddb0d2496ca1335c19370fbf14dd74dd51c4f46
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-07 12:24:03 +0300

    Frontend: Aligning left and right divs in the center

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -2,38 +2,43 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignIn = () => {
+  // Email and password use state
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   console.log(email);
   console.log(password);
 
   return (
-    <div id="register">
-      <div id="left-register"></div>
-      <div id="right-register">
-        <div>
-          <h1>Sign In</h1>
-          <label>Email</label>
-          <br />
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <br />
-          <label>Password</label>
-          <br />
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <br />
-          <button>Sign In</button>
-          <br />
-          <p>Don't have an account? {/*<Link>SignUp</Link> */}</p>
+    <div className="main-container">
+      <div className="register">
+        <div className="left-register"></div>
+        <div className="right-register">
+          {/* Sign in form */}
+          <div>
+            <h1>Sign In</h1>
+            <label>Email</label>
+            <br />
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+            <label>Password</label>
+            <br />
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <button>Sign In</button>
+            <br />
+            {/* Sign Up */}
+            <p>Don't have an account? {/*<Link>SignUp</Link> */}</p>
+          </div>
         </div>
       </div>
     </div>
