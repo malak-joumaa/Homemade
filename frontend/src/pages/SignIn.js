66ea8ea99cdec5123aad7a2f5a778bc04602056b
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-07 12:04:26 +0300

    Frontend: Adding useState and functions for the inputs on change

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -1,25 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  console.log(email);
+  console.log(password);
+
   return (
     <div id="register">
       <div id="left-register"></div>
       <div id="right-register">
-        <from>
+        <div>
           <h1>Sign In</h1>
           <label>Email</label>
           <br />
-          <input type="email" />
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <br />
           <label>Password</label>
           <br />
-          <input type="password" />
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <br />
           <button>Sign In</button>
           <br />
           <p>Don't have an account? {/*<Link>SignUp</Link> */}</p>
-        </from>
+        </div>
       </div>
     </div>
   );
