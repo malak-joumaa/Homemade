f37b93bfc13c49475bf56c4b3c9fa341b1ffe625
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-07 09:53:07 +0300

    Frontend: Adding routes and creating sign in components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./components/SignIn";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
