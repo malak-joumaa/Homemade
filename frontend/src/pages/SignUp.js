85cce4171e2e0c2b7609bb7ba179ad952eca610c
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-08 23:17:31 +0300

    Frontend: Adding useStates to sign-in page

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -3,6 +3,14 @@ import { Link } from "react-router-dom";
 import Logo from "../assets/logo.png";
 
 const SignUp = () => {
+  // Use states
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [c_password, setCpassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
   return (
     <div className="main-container">
       <div className="register sign-up">
@@ -16,28 +24,58 @@ const SignUp = () => {
           <div className="left-register left-sign-up">
             <label>First Name</label>
             <br />
-            <input type="text" id="f_name" />
+            <input
+              type="text"
+              id="fname"
+              value={fname}
+              onChange={(e) => setFname(e.target.value)}
+            />
             <br />
             <label>Last Name</label>
             <br />
-            <input type="text" id="l_name" />
+            <input
+              type="text"
+              id="lname"
+              value={lname}
+              onChange={(e) => setLname(e.target.value)}
+            />
             <br />
             <label>Email</label>
             <br />
-            <input type="email" id="email" />
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="right-register">
             <label>Password</label>
             <br />
-            <input type="password" id="password" />
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <br />
             <label>Confirm Password</label>
             <br />
-            <input type="password" id="c_password" />
+            <input
+              type="password"
+              id="c_password"
+              value={c_password}
+              onChange={(e) => setCpassword(e.target.value)}
+            />
             <br />
             <label>Phone number</label>
             <br />
-            <input type="text" id="phone_number" />
+            <input
+              type="text"
+              id="phone_number"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
           </div>
           <br />
           <button onClick={() => {}}>Sign Up</button>
