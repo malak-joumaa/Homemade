3d6200d54c70c50670a909b33dc736306a3038e9
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-08 21:03:09 +0300

    Frontend: Adding sign-up route in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
       </Routes>
     </BrowserRouter>
   );
