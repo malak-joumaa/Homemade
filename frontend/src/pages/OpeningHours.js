9c29efca873dcc5fbe0f882ea4100f75a2874aa4
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-09 21:10:43 +0300

    Frontend: Adding jsx

--- frontend/src/pages/OpeningHours.js
+++ frontend/src/pages/OpeningHours.js
@@ -2,7 +2,29 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const OpeningHours = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="follow-up-div">
+        {/* Title */}
+        <h1>Set Opening Hours</h1>
+
+        {/* Main Content */}
+        <div id="oph-photo"></div>
+        <div className="setTime">
+          From <input type="time" id="oph" name="oph" required />
+          Till <input type="time" id="oph" name="oph" required />
+        </div>
+
+        {/* Links */}
+        <Link to="/opening-hours" className="skip">
+          Skip
+        </Link>
+        <Link to="/opening-hours" className="next">
+          Next -{">"}
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default OpeningHours;
