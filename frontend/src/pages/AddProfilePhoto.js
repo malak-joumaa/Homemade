faf11e0d914390bda4eb3e3185a0422b498851ba
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-09 07:13:13 +0300

    Frontend: Styling elements in AddProfilePhoto page

--- frontend/src/pages/AddProfilePhoto.js
+++ frontend/src/pages/AddProfilePhoto.js
@@ -5,12 +5,16 @@ const AddProfilePhoto = () => {
   return (
     <div>
       <div className="follow-up-div">
-        <h2>Upload Profile Photo</h2>
+        <h1>Upload Profile Photo</h1>
         <input type="file" />
         <div id="uploaded-profile-photo"></div>
         {/* Link to be changed */}
-        <Link to="/sign-in">Skip</Link>
-        <Link to="/sign-in">Next -{">"}</Link>
+        <Link to="/sign-in" className="skip">
+          Skip
+        </Link>
+        <Link to="/sign-in" className="next">
+          Next -{">"}
+        </Link>
       </div>
     </div>
   );
