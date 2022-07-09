0f88b78d0e0f0821b166cfe58d60f2a4981ced45
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-09 10:24:59 +0300

    Frontend: Adding onChange listener functionality to file input

--- frontend/src/pages/AddProfilePhoto.js
+++ frontend/src/pages/AddProfilePhoto.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const AddProfilePhoto = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [imageSrc, setImageSrc] = useState("");
+  const [fileName, setFileName] = useState("");
+
   return (
     <div>
       <div className="follow-up-div">
         <h1>Upload Profile Photo</h1>
-        <input type="file" />
+
+        <div class="file-upload">
+          {!selectedImage ? (
+            <div class="image-upload-wrap">
+              <input
+                class="file-upload-input"
+                type="file"
+                onChange={(e) => {
+                  setSelectedImage(e.target.files[0]);
+                  setFileName(e.target.files[0].name);
+                  var reader = new FileReader();
+                  reader.onload = function (event) {
+                    setImageSrc(event.target.result);
+                    console.log(e.target.files[0]);
+                    console.log(event.target);
+                    console.log(event.target.result);
+                    console.log(imageSrc);
+                  };
+                  reader.readAsDataURL(e.target.files[0]);
+                }}
+                accept="image/*"
+              />
+              <h3>CLICK OR DRAG AND DROP A FILE</h3>
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+
         <div id="uploaded-profile-photo"></div>
         {/* Link to be changed */}
         <Link to="/sign-in" className="skip">
