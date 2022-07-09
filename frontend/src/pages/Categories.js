7488d4780a5dfca24ca6baaf58a2750f2596abef
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-10 00:00:29 +0300

    Frontend: Adding Category component

--- frontend/src/pages/Categories.js
+++ frontend/src/pages/Categories.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Caregories = () => {
-  return <div>Caregories</div>;
+  return (
+    <div>
+      <div className="follow-up-div">
+        {/* Title */}
+        <h1>Choose Preffered Categories</h1>
+        {/* Main Content */}
+
+        {/* Links */}
+        <Link to="/opening-hours" className="skip">
+          Skip
+        </Link>
+        <Link to="/opening-hours" className="next">
+          Next -{">"}
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Caregories;
