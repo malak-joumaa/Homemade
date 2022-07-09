6ff7fef524952e5e3b54ac5d851169b9e3805f9c
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-09 22:50:25 +0300

    Frontend: Styling OpeningHours page

--- frontend/src/pages/OpeningHours.js
+++ frontend/src/pages/OpeningHours.js
@@ -1,27 +1,53 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Photo from "../assets/hours.jpg";
 
 const OpeningHours = () => {
+  const [valid, setValid] = useState(false);
+
   return (
     <div>
       <div className="follow-up-div">
         {/* Title */}
         <h1>Set Opening Hours</h1>
+        <div className="oph-div">
+          {/* Main Content */}
+          <div id="oph-photo">
+            <img src={Photo} />
+          </div>
+          <div id="setTime">
+            From{" "}
+            <input
+              type="time"
+              className="oph"
+              name="oph"
+              required
+              id={!valid ? "" : "oph-validity"}
+              onClick={() => {
+                setValid(true);
+              }}
+            />
+            Till{" "}
+            <input
+              type="time"
+              className="oph"
+              name="oph"
+              required
+              id={!valid ? "" : "oph-validity"}
+              onClick={() => {
+                setValid(true);
+              }}
+            />
+          </div>
 
-        {/* Main Content */}
-        <div id="oph-photo"></div>
-        <div className="setTime">
-          From <input type="time" id="oph" name="oph" required />
-          Till <input type="time" id="oph" name="oph" required />
+          {/* Links */}
+          <Link to="/opening-hours" className="skip">
+            Skip
+          </Link>
+          <Link to="/opening-hours" className="next">
+            Next -{">"}
+          </Link>
         </div>
-
-        {/* Links */}
-        <Link to="/opening-hours" className="skip">
-          Skip
-        </Link>
-        <Link to="/opening-hours" className="next">
-          Next -{">"}
-        </Link>
       </div>
     </div>
   );
