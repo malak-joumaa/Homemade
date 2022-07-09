5ba164288356637485e747d57f8146f37ed8c45e
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-09 12:47:43 +0300

    Frontend: Adding useContext

--- frontend/src/pages/AddProfilePhoto.js
+++ frontend/src/pages/AddProfilePhoto.js
@@ -1,41 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const AddProfilePhoto = () => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [imageSrc, setImageSrc] = useState("");
   const [fileName, setFileName] = useState("");
+  const PhotoContext = createContext();
 
   return (
     <div>
-      <div className="follow-up-div">
-        <h1>Upload Profile Photo</h1>
+      <PhotoContext.Provider value={imageSrc}>
+        <div className="follow-up-div">
+          <h1>Upload Profile Photo</h1>
 
-        <div class="file-upload">
-          {!selectedImage ? (
-            <div class="image-upload-wrap">
-              <input
-                class="file-upload-input"
-                type="file"
-                onChange={(e) => {
-                  setSelectedImage(e.target.files[0]);
-                  setFileName(e.target.files[0].name);
-                  var reader = new FileReader();
-                  reader.onload = function (event) {
-                    setImageSrc(event.target.result);
-                    console.log(e.target.files[0]);
-                    console.log(event.target);
-                    console.log(event.target.result);
-                    console.log(imageSrc);
-                  };
-                  reader.readAsDataURL(e.target.files[0]);
-                }}
-                accept="image/*"
-              />
-              <h3>CLICK OR DRAG AND DROP A FILE</h3>
-            </div>
-          ) : (
-            <>
+          <div class="file-upload">
+            {!selectedImage ? (
+              <div class="image-upload-wrap">
+                <input
+                  class="file-upload-input"
+                  type="file"
+                  onChange={(e) => {
+                    setSelectedImage(e.target.files[0]);
+                    setFileName(e.target.files[0].name);
+                    var reader = new FileReader();
+                    reader.onload = function (event) {
+                      setImageSrc(event.target.result);
+                      console.log(e.target.files[0]);
+                      console.log(event.target);
+                      console.log(event.target.result);
+                      console.log(imageSrc);
+                    };
+                    reader.readAsDataURL(e.target.files[0]);
+                  }}
+                  accept="image/*"
+                />
+                <h3>CLICK OR DRAG AND DROP A FILE</h3>
+              </div>
+            ) : (
               <div class="file-upload-content">
                 <img
                   class="file-upload-image"
@@ -56,19 +57,19 @@ const AddProfilePhoto = () => {
                   </button>
                 </div>
               </div>
-            </>
-          )}
-        </div>
+            )}
+          </div>
 
-        <div id="uploaded-profile-photo"></div>
-        {/* Link to be changed */}
-        <Link to="/sign-in" className="skip">
-          Skip
-        </Link>
-        <Link to="/sign-in" className="next">
-          Next -{">"}
-        </Link>
-      </div>
+          <div id="uploaded-profile-photo"></div>
+          {/* Link to be changed */}
+          <Link to="/sign-in" className="skip">
+            Skip
+          </Link>
+          <Link to="/sign-in" className="next">
+            Next -{">"}
+          </Link>
+        </div>
+      </PhotoContext.Provider>
     </div>
   );
 };
