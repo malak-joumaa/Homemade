6d67549f571f3c27462aa6ea4de2adbfd6144dcd
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-10 23:20:38 +0300

    Backend: Including add-dish and add-menu routes

--- backend/src/menu/index.js
+++ backend/src/menu/index.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
-const { addCategory } = require("./controller/menu");
+const { addCategory, addMenu, addDish } = require("./controller/menu");
 const router = Router();
 
 router.post("/auth/add-category", addCategory);
+router.post("/auth/add-menu", addMenu);
+router.post("/auth/add-dish", addDish);
 
 module.exports = router;
