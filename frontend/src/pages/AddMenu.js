f323783121dc246ef2e37eb64462c805a825c99f
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-11 22:50:08 +0300

    Frontend: Styling labels

--- frontend/src/pages/AddMenu.js
+++ frontend/src/pages/AddMenu.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Specifications from "../components/Specifications";
+import "../styles/menu.css";
 
 const AddMenu = () => {
   const [dish, setDish] = useState([
@@ -34,48 +35,56 @@ const AddMenu = () => {
   };
 
   return (
-    <div>
+    <div id="menu">
       <div id="add-menu">
         <form>
           {/* Adding a menu item(dish) */}
           {dish.map((input, index) => (
-            <div key={index}>
-              <h3>Item Name:</h3>
-              <input
-                type="text"
-                name="name"
-                value={input.name}
-                onChange={(e) => {
-                  handleFormChange(index, e);
-                }}
-              />
-              <p>Add photo</p>
-              <input type="file" />
-              <p>Description</p>
-              <input
-                type="text"
-                name="description"
-                value={input.description}
-                onChange={(e) => {
-                  handleFormChange(index, e);
-                }}
-              />
-              <p>Price</p>
-              <input
-                type="text"
-                name="price"
-                value={input.price}
-                onChange={(e) => {
-                  handleFormChange(index, e);
-                }}
-              />
-              <p>Quantity</p>
-              <input type="text" />
-              <p>Category</p>
-              <input type="text" />
-
-              <h3>Specifications</h3>
-              <Specifications />
+            <div key={index} id="dish">
+              <div>
+                <h3>Item Name:</h3>
+                <input
+                  type="text"
+                  name="name"
+                  value={input.name}
+                  onChange={(e) => {
+                    handleFormChange(index, e);
+                  }}
+                />
+                <br />
+                <span>Add photo:</span>
+                <input type="file" />
+                <br />
+                <span>Description:</span>
+                <input
+                  type="text"
+                  name="description"
+                  value={input.description}
+                  onChange={(e) => {
+                    handleFormChange(index, e);
+                  }}
+                />
+                <br />
+                <span>Price:</span>
+                <input
+                  type="text"
+                  name="price"
+                  value={input.price}
+                  onChange={(e) => {
+                    handleFormChange(index, e);
+                  }}
+                />
+                <br />
+                <span>Quantity:</span>
+                <input type="text" />
+                <br />
+                <span>Category:</span>
+                <input type="text" />
+              </div>
+              <div>
+                <h3>Specifications</h3>
+                <Specifications />
+              </div>
             </div>
           ))}
           <button
