0ca6b7b6ad7a62b77ddff6df7b86cd4d9ec7e5f2
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-11 10:43:25 +0300

    Adding useState and jsx to AddMenu page

--- frontend/src/pages/AddMenu.js
+++ frontend/src/pages/AddMenu.js
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddMenu = () => {
-  return <div>AddMenu</div>;
+  const [dish, setDish] = useState([
+    {
+      name: "",
+      description: "",
+      price: "",
+      quantity: "",
+      photo: "",
+      categories: [],
+    },
+  ]);
+
+  const handleFormChange = (index, event) => {
+    let data = [...dish];
+    data[index][event.target.name] = event.target.value;
+    setDish(data);
+  };
+  console.log(dish);
+
+  return (
+    <div>
+      <div id="add-menu">
+        <form>
+          {/* Adding a menu item(dish) */}
+          {dish.map((input, index) => (
+            <div key={index}>
+              <h3>Item Name:</h3>
+              <input
+                type="text"
+                name="name"
+                value={input.name}
+                onChange={(e) => {
+                  handleFormChange(index, e);
+                }}
+              />
+              <p>Add photo</p>
+              <input type="file" />
+              <p>Description</p>
+              <input
+                type="text"
+                name="description"
+                value={input.description}
+                onChange={(e) => {
+                  handleFormChange(index, e);
+                }}
+              />
+              <p>Price</p>
+              <input
+                type="text"
+                name="price"
+                value={input.price}
+                onChange={(e) => {
+                  handleFormChange(index, e);
+                }}
+              />
+              <p>Quantity</p>
+              <input type="text" />
+              <p>Category</p>
+              <input type="text" />
+
+              <h3>Special Instructions</h3>
+            </div>
+          ))}
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddMenu;
