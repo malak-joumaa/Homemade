673641de9b43da8bf8f0c44f4a843054f1b1eb53
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-18 13:31:13 +0300

    Fontend: Adding redux persist to react store and index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./state/store";
+import store, { Persistor } from "./state/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={Persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -1,5 +1,19 @@
 import { createStore, applyMiddleware } from "redux";
 import reducers from "./reducers";
 import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = createStore(persistedReducer, {}, applyMiddleware(thunk));
+
+const Persistor = persistStore(store);
+
+export { Persistor };
+export default store;
