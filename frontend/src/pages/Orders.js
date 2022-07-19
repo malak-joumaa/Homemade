2d5fb2e73a324c479f352b5360107cfaa6189f8d
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-19 17:58:52 +0300

    Frontend: Adding and styling order in Orders page

--- frontend/src/pages/Orders.js
+++ frontend/src/pages/Orders.js
@@ -1,13 +1,60 @@
+import { Grid } from "@mui/material";
 import { Container } from "@mui/system";
 import React from "react";
 import Navbar from "../components/Navbar";
-import { Day } from "../styles/SingleCook.styles";
+import {
+  Nav,
+  NavItem,
+  CartOrder,
+  Photo,
+  CookName,
+  Answer,
+  CookInfo,
+  Info,
+  Delete,
+} from "../styles/Orders.style";
 
 const Orders = () => {
   return (
     <>
       <Container maxWidth="xl">
         <Navbar />
+        <Nav>
+          <NavItem>Cart</NavItem>
+          <NavItem>Pre-Ordered</NavItem>
+        </Nav>
+        <CartOrder>
+          <Grid container spacing={1}>
+            <Grid item xs={0.8}>
+              <Photo />
+            </Grid>
+            <Grid item xs={7.2}>
+              <CookInfo>
+                <CookName>Cook Name</CookName>
+                <br />
+                <span>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Aliquam rem officia, corrupti minima nisi modi.{" "}
+                  <Answer>Spicy</Answer>
+                </span>
+              </CookInfo>
+            </Grid>
+            <Grid item xs={1}>
+              <Info>Chef's name</Info>
+            </Grid>
+            <Grid item xs={1}>
+              <Info>3 items</Info>
+            </Grid>
+            <Grid item xs={1}>
+              <Info>Total:15$</Info>
+            </Grid>
+            <Grid item xs={1} align="center">
+              <Delete>
+                <i class="fa-solid fa-trash"></i>
+              </Delete>
+            </Grid>
+          </Grid>
+        </CartOrder>
       </Container>
     </>
   );
--- frontend/src/styles/Orders.Style.js
+++ frontend/src/styles/Orders.Style.js
@@ -1,1 +1,55 @@
 import styled from "styled-components";
+
+export const Nav = styled.div`
+  margin-top: 30px;
+  display: flex;
+`;
+
+export const NavItem = styled.div`
+  cursor: pointer;
+  border: 1px solid #79c3a1;
+  border-radius: 10px;
+  padding: 2px 11px;
+  margin-right: 20px;
+  font-size: 1.2em;
+  font-weight: 600;
+  &:hover {
+    background-color: #acdac5;
+  }
+`;
+
+export const CartOrder = styled.div`
+  margin-top: 40px;
+  display: flex;
+  height: 80px;
+  background-color: #f5f5f5;
+  border-radius: 5px;
+`;
+
+export const CookInfo = styled.div`
+  margin-top: 5px;
+`;
+
+export const Photo = styled.img`
+  height: 80px;
+  width: 80px;
+  border: 1px solid #79c3a1;
+  border-radius: 5px;
+`;
+
+export const CookName = styled.span`
+  font-weight: 600;
+`;
+
+export const Answer = styled.span`
+  color: #43c187;
+`;
+
+export const Info = styled.span`
+  line-height: 80px;
+`;
+
+export const Delete = styled.span`
+  line-height: 80px;
+  font-size: 1.2em;
+`;
