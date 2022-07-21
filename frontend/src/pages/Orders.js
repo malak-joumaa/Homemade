bc5a5d2f33030fb8774acd2e12970a3ba2ea713b
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-21 23:39:54 +0300

    Frontend: Linking get-orders api in Orders page

--- frontend/src/pages/Orders.js
+++ frontend/src/pages/Orders.js
@@ -1,6 +1,6 @@
 import { Grid } from "@mui/material";
 import { Container } from "@mui/system";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Cart from "../components/Cart";
 import Navbar from "../components/Navbar";
 import Ordered from "../components/Ordered";
@@ -9,6 +9,28 @@ import { Nav, NavItem, Button } from "../styles/Orders.style";
 const Orders = () => {
   const [isCart, setCart] = useState(true);
   const [isOrdered, setOrdered] = useState(false);
+  const [order, setOrder] = useState([]);
+
+  useEffect(() => {
+    getOrders();
+  }, []);
+  console.log(order);
+
+  // Get Orders
+  const getOrders = async () => {
+    try {
+      const res = await fetch(
+        "http://localhost:5000/api/order/get-orders/?id=" +
+          localStorage.getItem("customer_id")
+      ).then(async (res) => {
+        const data = await res.json();
+        setOrder(data);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       <Container maxWidth="xl">
