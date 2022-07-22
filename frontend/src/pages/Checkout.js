b0e04f943508ba5247366e102da8522a38ef15ae
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-23 00:10:40 +0300

    Frontend: Styling and including more elements

--- frontend/src/pages/Checkout.js
+++ frontend/src/pages/Checkout.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import { Container } from "@mui/system";
-import { Title, Orders, Total } from "../styles/Checkout.styles";
+import {
+  Title,
+  Orders,
+  Total,
+  SubTitle,
+  Button,
+} from "../styles/Checkout.styles";
 import SingleOrder from "../components/SingleOrder";
 import { useSelector } from "react-redux";
 
 const Checkout = () => {
   const orderData = useSelector((state) => state.order);
+  const [time, setTime] = useState("");
+  const [isValid, setIsValid] = useState(false);
+
   console.log(orderData);
 
   var total = 0;
@@ -16,7 +25,7 @@ const Checkout = () => {
   return (
     <Container maxWidth="xl">
       <Navbar />
-      <Title>Chekout</Title>
+      <Title>Checkout</Title>
       <Orders>
         {orderData.map((order, index) => (
           <>
@@ -25,6 +34,25 @@ const Checkout = () => {
         ))}
       </Orders>
       <Total>Total: {total}$</Total>
+      <SubTitle>Location:</SubTitle>
+      <SubTitle>Pickup Hour:</SubTitle>
+      <input
+        type="time"
+        className="oph"
+        name="oph"
+        value={time}
+        onChange={(e) => {
+          setTime(e.target.value);
+        }}
+        required
+        id={!isValid ? "" : "oph-validity"}
+        onClick={(e) => {
+          setIsValid(true);
+        }}
+        style={{ marginLeft: "0px" }}
+      />
+      <br />
+      <Button>Confirm</Button>
     </Container>
   );
 };
--- frontend/src/styles/Checkout.styles.js
+++ frontend/src/styles/Checkout.styles.js
@@ -5,6 +5,12 @@ export const Title = styled.h2`
   color: #4f4f4f;
   padding-top: 10px;
 `;
+
+export const SubTitle = styled.h3`
+  color: #4f4f4f;
+  font-size: 1.3em;
+`;
+
 export const Orders = styled.div`
 width:100%
 min-height:80px;
@@ -16,5 +22,16 @@ background-color: #f5f5f5;
 export const Total = styled.h3`
   text-align: center;
   padding: 7px 12px;
-  background-color: #76cfa6;
+  background-color: #f5f5f5;
+`;
+
+export const Button = styled.button`
+  padding: 7px 15px;
+  font-size: 1.25em;
+  background-color: white;
+  border: 2px solid #79c3a1;
+  border-radius: 10px;
+  margin-top: 30px;
+  display: block;
+  margin: 0 auto;
 `;
