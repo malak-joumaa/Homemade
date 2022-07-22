124fbc373de072fd63255a3334d1e096a22019e1
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-23 02:08:24 +0300

    Frontend: Adding Navbar and Container

--- frontend/src/pages/CookProfile.js
+++ frontend/src/pages/CookProfile.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import { Rating, Grid, Container } from "@mui/material";
 
 const CookProfile = () => {
-  return <div>CookProfile</div>;
+  return (
+    <Container maxWidth="xl">
+      <Navbar />
+    </Container>
+  );
 };
 
 export default CookProfile;
