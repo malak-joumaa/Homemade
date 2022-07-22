e727866e382f76b62b690dea8b61a76d2c1c66dc
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-23 02:13:48 +0300

    Frontend: Retrieving user data from redux store

--- frontend/src/pages/CookProfile.js
+++ frontend/src/pages/CookProfile.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { Rating, Grid, Container } from "@mui/material";
+import { useSelector } from "react-redux";
 
 const CookProfile = () => {
+  const userData = useSelector((state) => state.login);
+  console.log(userData);
   return (
     <Container maxWidth="xl">
       <Navbar />
