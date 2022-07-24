fef473f2e3a274b9b7dcc9b86cae720b2c3fdc1f
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-24 12:56:30 +0300

    Frontend: Inserting and styling main chat divs

--- frontend/src/pages/chat/Chat.js
+++ frontend/src/pages/chat/Chat.js
@@ -1,10 +1,41 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
+import { Grid, Container } from "@mui/material";
+import {
+  ChatContainer,
+  ChatMenu,
+  ChatOnline,
+  ChatBox,
+  ChatMenuWrapper,
+  ChatBoxWrapper,
+  ChatOnlineWrapper,
+} from "../../styles/chat/Chat.style";
 
 const Chat = () => {
   return (
     <div>
       <Navbar />
+      <ChatContainer>
+        <Grid container spacing={1}>
+          <Grid item xs={3.5}>
+            <ChatMenu>
+              <ChatMenuWrapper>menu</ChatMenuWrapper>
+            </ChatMenu>
+          </Grid>
+
+          <Grid item xs={5.5}>
+            <ChatBox>
+              <ChatBoxWrapper>box</ChatBoxWrapper>
+            </ChatBox>
+          </Grid>
+
+          <Grid item xs={3}>
+            <ChatOnline>
+              <ChatOnlineWrapper>wrapper</ChatOnlineWrapper>
+            </ChatOnline>
+          </Grid>
+        </Grid>
+      </ChatContainer>
     </div>
   );
 };
--- frontend/src/styles/chat/Chat.style.js
+++ frontend/src/styles/chat/Chat.style.js
@@ -1,1 +1,27 @@
 import styled from "styled-components";
+
+export const ChatContainer = styled.div`
+  height: calc(100vh - 70px);
+  display: flex;
+`;
+
+export const ChatMenu = styled.div``;
+
+export const ChatOnline = styled.div``;
+
+export const ChatBox = styled.div``;
+
+export const ChatMenuWrapper = styled.div`
+  padding: 10px;
+  height: calc(100vh - 100px);
+`;
+
+export const ChatOnlineWrapper = styled.div`
+  padding: 10px;
+  height: calc(100vh - 100px);
+`;
+
+export const ChatBoxWrapper = styled.div`
+  padding: 10px;
+  height: calc(100vh - 100px);
+`;
