9926d7a95b0f7cc93774c782908d3b9ad4068dde
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-24 20:00:45 +0300

    Getting chat by user_id, mapping over conversation component and sending props to it

--- frontend/src/pages/chat/Chat.js
+++ frontend/src/pages/chat/Chat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import { Grid, Container } from "@mui/material";
 import {
@@ -18,8 +18,33 @@ import {
 import Conversation from "./Conversation";
 import Message from "./Message";
 import OnlineChat from "./OnlineChat";
+import { useSelector } from "react-redux";
 
 const Chat = () => {
+  const user = useSelector((state) => state.login);
+  const [conversation, setConversation] = useState([]);
+  const user_id = user.user_id;
+
+  console.log(user_id);
+  useEffect(() => {
+    getConversations();
+  }, []);
+
+  const getConversations = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:5000/api/chat/get-convo/?id=" + user_id
+      );
+      const data = await response.json();
+      console.log(data);
+      setConversation(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  console.log("convo", conversation);
+
   return (
     <div>
       <Navbar />
@@ -29,10 +54,9 @@ const Chat = () => {
             <ChatMenu>
               <ChatMenuWrapper>
                 <SearchBox placeholder="search" />
-                <Conversation />
-                <Conversation />
-                <Conversation />
-                <Conversation />
+                {conversation.map((convo) => (
+                  <Conversation conversation={convo} currentUser={user_id} />
+                ))}
               </ChatMenuWrapper>
             </ChatMenu>
           </Grid>
