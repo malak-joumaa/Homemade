8a428a1e02062d72323e37cf8e7c90865ca52fa1
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-24 07:01:14 +0300

    Adding first section of landing page

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,7 +1,38 @@
 import React from "react";
+import {
+  Blob,
+  MainLogo,
+  Container1,
+  Welcome,
+  SubTitle,
+  SignInBtn,
+  SignInText,
+  TypeText,
+} from "../styles/LandingPage.style";
+import logo from "../assets/3.png";
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <Container1>
+      <Blob>
+        <Welcome>Welcome to Homemade</Welcome>
+        <SubTitle>For Home Cooks</SubTitle>
+        <SubTitle>For Food Lovers</SubTitle>
+        <SignInBtn>
+          <SignInText>Sign in</SignInText>
+          <br />
+
+          <TypeText>As a customer</TypeText>
+        </SignInBtn>
+        <SignInBtn>
+          <SignInText>Sign in</SignInText>
+          <br />
+          <TypeText>As a cook</TypeText>
+        </SignInBtn>
+      </Blob>
+      <MainLogo src={logo} />
+    </Container1>
+  );
 };
 
 export default LandingPage;
