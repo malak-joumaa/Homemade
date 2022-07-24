125add91c37d1d0bd13cdadc06d0cd00481951a3
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-24 12:29:31 +0300

    Frontend: Adding Navbar to Chat page

--- frontend/src/pages/chat/Chat.js
+++ frontend/src/pages/chat/Chat.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "../../components/Navbar";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default Chat;
