e2415ca5d3cec3256ac64406b3a0ba0fe391491d
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-24 21:01:00 +0300

    Frontend: Getting friend data and displaying his name and profile picture

--- frontend/src/pages/chat/Conversation.js
+++ frontend/src/pages/chat/Conversation.js
@@ -10,13 +10,29 @@ const Conversation = ({ conversation, currentUser }) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const friendId = conversation.member.find((m) => m !== currentUser);
-  }, []);
+    const friendId = conversation.members.find((m) => m !== currentUser);
+    console.log(friendId);
+    const getUser = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:5000/api/user/auth/get-user/?id=" + friendId
+        );
+        const data = await response.json();
+        console.log("friend", data);
+        setUser(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getUser();
+  }, [currentUser, conversation]);
 
   return (
     <ConversationContainer>
-      <ConversationImg src={userProfile}></ConversationImg>
-      <ConversationName>Malak Joumaa</ConversationName>
+      <ConversationImg src={user ? user.profile_photo : ""}></ConversationImg>
+      <ConversationName>
+        {user ? user.first_name : "Friend"} {user ? user.last_name : "Friend"}
+      </ConversationName>
     </ConversationContainer>
   );
 };
