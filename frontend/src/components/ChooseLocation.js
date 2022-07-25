0fc6e552a1a33f8cac8d7a8d4b61e66b3d0894a7
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-25 19:03:55 +0300

    Frontend: Removing react leaflet map and using google maps instead

--- frontend/src/components/ChooseLocation.js
+++ frontend/src/components/ChooseLocation.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Maps from "./maps/Maps";
+import { useJsApiLoader, GoogleMap, Marker } from "@react-google-maps/api";
 
 function ChooseLocation() {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
+
+  if (!isLoaded) {
+    return <span>Loading...</span>;
+  }
+  const center = { lat: 33.893791, lng: 35.501778 };
   return (
     <>
       <h1>Choose Location</h1>
-      <Maps />
+      <div style={{ width: "100%", height: "80%" }}>
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: "100%", height: "100%" }}
+        >
+          <Marker position={center} />
+        </GoogleMap>
+      </div>
     </>
   );
 }
--- frontend/src/components/maps/Maps.js
+++ frontend/src/components/maps/Maps.js
@@ -1,29 +1,7 @@
 import React, { useState } from "react";
-import {
-  MapContainer,
-  Marker,
-  TileLayer,
-  useMap,
-  useMapEvents,
-} from "react-leaflet";
 
 const Maps = () => {
-  return (
-    <>
-      <MapContainer
-        center={[33.893791, 35.501778]}
-        zoom={13}
-        scrollWheelZoom={false}
-        style={{ width: "100%", height: "80%" }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[33.893791, 35.501778]}></Marker>
-      </MapContainer>
-    </>
-  );
+  return <></>;
 };
 
 export default Maps;
