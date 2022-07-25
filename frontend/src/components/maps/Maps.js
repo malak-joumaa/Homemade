19d9dca768ba593cca18f00dbe2879d4ff7a9117
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-25 16:29:11 +0300

    Adding react leaflet map

--- frontend/src/components/maps/Maps.js
+++ frontend/src/components/maps/Maps.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  MapContainer,
+  Marker,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
 
 const Maps = () => {
-  return <div>Maps</div>;
+  return (
+    <>
+      <MapContainer
+        center={[33.893791, 35.501778]}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ width: "100%", height: "80%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[33.893791, 35.501778]}></Marker>
+      </MapContainer>
+    </>
+  );
 };
 
 export default Maps;
