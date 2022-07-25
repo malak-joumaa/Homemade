048d524189ca7297ce583e044a93c8a0a9efe9ae
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-07-25 19:35:02 +0300

    Frontend: Creating MapSearchBox component with Map.style file

---
+++ frontend/src/components/maps/MapSearchBox.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MapSearchBox = () => {
+  return <div>MapSearchBox</div>;
+};
+
+export default MapSearchBox;
--- frontend/src/components/maps/Maps.js
+++ frontend/src/components/maps/Maps.js
@@ -1,7 +1,29 @@
 import React, { useState } from "react";
+import { useJsApiLoader, GoogleMap, Marker } from "@react-google-maps/api";
 
 const Maps = () => {
-  return <></>;
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
+
+  if (!isLoaded) {
+    return <span>Loading...</span>;
+  }
+  const center = { lat: 33.893791, lng: 35.501778 };
+  return (
+    <>
+      <div style={{ width: "100%", height: "80%" }}>
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: "100%", height: "100%" }}
+          onLoad={(map) => setMap(map)}
+        >
+          <Marker position={center} />
+        </GoogleMap>
+      </div>
+    </>
+  );
 };
 
 export default Maps;
